7b83905489a9bcc43877d909d6a50dfcda41091c
trgandhi <trgandhi1@gmail.com>
2023-11-21 13:32:02 -0600

    updated rppa aggregation script
    now script trims whitespace from anitbody id, antibody name and gene symbols.
    
    script also adds additional sheets/tabs in the complete report file showing median normalized data for phospho pairs and epigenetic modifiers in separate sheets/tabs.
    
    script also added antibody ids to the data in the norm_median, norm_cv and other such sheets/tabs.

--- rppaAggr.tg.R
+++ rppaAggr.tg.R
@@ -15,13 +15,16 @@ rppaTool <- function(inputFile,
                        sampleIDRow,
                        replacement) {
     mydf <- read.xlsx(inputFile, sheet = sheetName)
+    
+    # trimming any trailing whitespaces from antibody IDs, antibody names and gene symbols
+    mydf[,c(1:3)] <- apply(mydf[,c(1:3)], 2, trimws)
+    
     mygroups <- data.frame(ID=unname(sapply(colnames(mydf)[-c(1:5)], function(x){strsplit(x,"[.]")[[1]][1]})),
                            Sample=t(mydf[1,-c(1:5)]))
     
-    tempIDs <- mydf$X1[-1]
-    mydf <- data.frame(mydf[-1,-c(1,3,5)], row.names = NULL, check.rows = F, check.names = F)
-    mydf[,1] <- gsub("_R_V","",mydf[,1])
-    mydf[,2] <- unname(sapply(mydf[,2], function(x){strsplit(x, ",")[[1]][1]}))
+    mydf <- data.frame(mydf[-1,-c(3,5)], row.names = NULL, check.rows = F, check.names = F)
+    mydf[,2] <- gsub("_R_V","",mydf[,2])
+    mydf[,3] <- unname(sapply(mydf[,3], function(x){strsplit(x, ",")[[1]][1]}))
     
     # Function to add ID to duplicate names
     makeUniqueNames <- function(IDs, names) {
@@ -48,37 +51,21 @@ rppaTool <- function(inputFile,
       return(unique_names)
     }
     
-    mydf$AB_name <- makeUniqueNames(IDs = tempIDs, names = mydf$AB_name)
+    mydf$AB_name <- makeUniqueNames(IDs = mydf$X1, names = mydf$AB_name)
     
-    mydf[,-c(1,2)] <- sapply(mydf[,-c(1,2)], as.numeric)
+    mydf[,-c(1,2,3)] <- sapply(mydf[,-c(1,2,3)], as.numeric)
     mydf[is.na(mydf)] <- 1
     
-    tempdf <- data.frame(Sample = mygroups[,sampleIDRow], t(data.frame(mydf[,-2], row.names = 1, check.rows = F, check.names = F)), check.names = F, check.rows = F)
+    tempdf <- data.frame(Sample = mygroups[,sampleIDRow], t(data.frame(mydf[,-c(1,3)], row.names = 1, check.rows = F, check.names = F)), check.names = F, check.rows = F)
     newdf <- aggregate(. ~ Sample, data = tempdf, FUN = median)
     cvdf <- aggregate(. ~ Sample, data = tempdf, FUN = function(x){sd(x)/mean(x)})
     cvdf.1 <- cvdf[,-1]
-    if (!(is.null(replacement))) {
-      newdf.1 <- newdf[,-1]
-      newdf.1[cvdf.1 > cv_cutoff] <- replacement
-      newdf[,-1] <- newdf.1
-    }
     
-    # if (sampleIDRow == 1) {
-    #   uniqueNames <- data.frame(ID = unname(sapply(mygroups$ID, function(x){strsplit(x,"[.]")[[1]][1]})),
-    #                             sample = mygroups$Sample)
-    #   uniqueNames <- uniqueNames[!duplicated(uniqueNames$sample),]
-    #   uniqueNames <- uniqueNames[match(newdf$Sample, uniqueNames$sample),]
-    #   if (identical(newdf$Sample, uniqueNames$sample)) {
-    #     newdf$Sample <- uniqueNames$ID 
-    #   }
-    # }
-    
-    # write.xlsx(list(rppa = newdf), paste0(outdir, "/aggregate_data.xlsx"), rowNames = F) 
     newdf <- data.frame(newdf, row.names = 1, check.rows = F, check.names = F)
     rownames(cvdf.1) <- rownames(newdf)
     
-    finaldf <- data.frame(GeneSymbol = mydf[,2], AB_name = colnames(newdf), t(newdf), check.rows = F, check.names = F)
-    finaldf.cv <- data.frame(GeneSymbol = mydf[,2], AB_name = colnames(cvdf.1), t(cvdf.1), check.rows = F, check.names = F)
+    finaldf <- data.frame(AB_ID = as.numeric(mydf[,1]), AB_name = colnames(newdf), GeneSymbol = mydf[,3], t(newdf), check.rows = F, check.names = F)
+    finaldf.cv <- data.frame(AB_ID = as.numeric(mydf[,1]), AB_name = colnames(newdf), GeneSymbol = mydf[,3], t(cvdf.1), check.rows = F, check.names = F)
     # write.xlsx(list(rppa = finaldf), paste0(outdir, "/full_aggregate_data.xlsx"), rowNames = F)
     
     addWorksheet(wb,paste0(sheetName,"_Median"))
@@ -86,7 +73,7 @@ rppaTool <- function(inputFile,
     addStyle(wb = wb,
              sheet = paste0(sheetName,"_Median"),
              style = createStyle(numFmt = "0.00"),
-             cols = 3:ncol(finaldf.cv),
+             cols = 4:ncol(finaldf.cv),
              rows = 2:(nrow(finaldf.cv)+1),
              gridExpand = T)
     
@@ -95,14 +82,14 @@ rppaTool <- function(inputFile,
     mystyle <- createStyle(fontColour = "#000000", bgFill = "#FFFF00")
     conditionalFormatting(wb,
                           paste0(sheetName,"_CV"),
-                          cols = 3:ncol(finaldf.cv),
+                          cols = 4:ncol(finaldf.cv),
                           rows = 2:(nrow(finaldf.cv)+1),
                           rule = paste0(">",cv_cutoff),
                           style = mystyle)
     addStyle(wb = wb,
              sheet = paste0(sheetName,"_CV"),
              style = createStyle(numFmt = "PERCENTAGE"),
-             cols = 3:ncol(finaldf.cv),
+             cols = 4:ncol(finaldf.cv),
              rows = 2:(nrow(finaldf.cv)+1),
              gridExpand = T)
     
@@ -124,7 +111,32 @@ rppaTool <- function(inputFile,
                     sampleIDRow = sampleIDRow,
                     replacement = replacement)
   }
+  tempdf.0 <- readWorkbook(wb1, sheet = "Norm_Median", rowNames = T)
   
+  ab_file <- "~/Box/gitrepos/rppaTool/data/RPPA0054_Antibody_list-clean.xlsx"
+  new.names <- excel_sheets(ab_file)
+  
+  for (idx in seq_along(new.names)) {
+    ab_list <- read.xlsx(ab_file, sheet = new.names[idx], rowNames = T)
+    keep <- intersect(rownames(ab_list), rownames(tempdf.0))
+    
+    df.new <- tempdf.0[keep,]
+    df.new <- data.frame(AB_ID=as.numeric(rownames(df.new)),
+                         df.new,
+                         check.rows = F,
+                         check.names = F)
+    
+    sheetName <- paste0("Norm_Median_", new.names[idx])
+    addWorksheet(wb,sheetName)
+    writeData(wb,sheetName,df.new,rowNames = FALSE)
+    addStyle(wb = wb,
+             sheet = sheetName,
+             style = createStyle(numFmt = "0.00"),
+             cols = 4:ncol(df.new),
+             rows = 2:(nrow(df.new)+1),
+             gridExpand = T) 
+  }
+  browser()
   saveWorkbook(wb,outputFile,overwrite = TRUE)
 }
 
