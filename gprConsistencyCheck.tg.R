f816d2388cbda1286c951e2604c972184e1e9ae2
trgandhi <trgandhi1@gmail.com>
2024-05-09 09:37:38 -0500

    Update gprConsistencyCheck.tg.R
    updated the consistency check script so now, for each gpr file, the script removes any empty blank lines or quotation marks present before saving the gpr file and also shows the number of quotation marks and empty blank lines removed in the consistency check report. The script has also been updated to check for F532 field in total protein gpr files and check for F635 field in all remaining gpr files since this has also caused issues with normalization.

--- gprConsistencyCheck.tg.R
+++ gprConsistencyCheck.tg.R
@@ -47,9 +47,35 @@ readOneGpr <- function(input_file) {
   # Close the file
   close(con)
   
-  num_lines <- length(readLines(input_file))
+  # Read in gpr file
+  temp <- readLines(input_file, warn = FALSE)
   
-  full_info <- c(input_file, basename(input_file), tiff_file, jpeg_file, num_lines)
+  # Check for F532 field in total protein gpr files and F635 field in other antibody gpr files
+  if (grepl("protein", input_file, ignore.case = T)) {
+    F532_F635 <- any(grepl("F532",temp))
+  } else {
+    F532_F635 <- any(grepl("F635",temp))
+  }
+  
+  # Count number of quotation marks to be removed
+  quotesRemoved <- sum(grepl('"',temp))
+  
+  # Remove quotation marks
+  temp.final <- gsub('"', '', temp)
+  
+  # Count number of empty blank lines to be removed
+  blankLinesRemoved <- sum(temp.final == "")
+  
+  # Remove blank lines
+  temp.final <- temp.final[!(temp.final == "")]
+  
+  # Save number of lines
+  num_lines <- length(temp.final)
+  
+  # Save final gpr file after removing quotation marks and blank lines
+  writeLines(temp.final, input_file)
+  
+  full_info <- c(input_file, basename(input_file), tiff_file, jpeg_file, F532_F635, quotesRemoved, blankLinesRemoved, num_lines)
   
   return(full_info)
 }
@@ -67,7 +93,7 @@ gprConsistencyCheck <- function(input_path = ".",
     finaldf <- rbind(finaldf, readOneGpr(myfile))
   }
   finaldf <- as.data.frame(finaldf)
-  finaldf$V5 <- as.numeric(finaldf$V5)
+  finaldf$V8 <- as.numeric(finaldf$V8)
   
   # function to get statistical mode of the input
   getmode <- function(myvec) {
@@ -76,33 +102,37 @@ gprConsistencyCheck <- function(input_path = ".",
   }
   
   # get mode of number of lines in gpr files
-  mode_num_lines <- getmode(finaldf[,5])
+  mode_num_lines <- getmode(finaldf[,8])
   
-  finaldf$Consistent <- sapply(1:nrow(finaldf), function(x){
+  finaldf$Consistency <- sapply(1:nrow(finaldf), function(x){
     input_file <- finaldf[x,1]
     tiff_file <- finaldf[x,3]
     jpeg_file <- finaldf[x,4]
-    num_lines <- finaldf[x,5]
+    F532_F635 <- finaldf[x,5]
+    num_lines <- finaldf[x,8]
     if((file_path_sans_ext(basename(input_file)) == file_path_sans_ext(basename(tiff_file))) && (file_path_sans_ext(basename(input_file)) == file_path_sans_ext(basename(jpeg_file)))) {
       if (grepl("protein", input_file, ignore.case = T)) {
         temp.list <- unlist(strsplit(basename(input_file),"_"))
         temp.idx <- which(grepl("protein",temp.list,ignore.case = T))
         if (gsub("[^0-9]", "",temp.list[temp.idx-1]) == gsub("[^0-9]", "",temp.list[temp.idx+1])) {
-          consistency <- "TRUE"
+          consistency <- "PASS"
         } else { 
-          consistency <- "FALSE (protein name and slide number not consistent)"
+          consistency <- "FAIL (protein name and slide number not consistent)"
         }
-      } else {consistency <- "TRUE"}
+      } else {consistency <- "PASS"}
     } else { 
-      consistency <- "FALSE (input, tiff and jpeg file names not matching)"
+      consistency <- "FAIL (input, tiff and jpeg file names not matching)"
     }
     if (num_lines != mode_num_lines) {
-      consistency <- "FALSE (number of lines not matching)"
+      consistency <- "FAIL (number of lines not matching)"
+    }
+    if (!(as.logical(F532_F635))) {
+      consistency <- "FAIL (F532/F635 field missing or mislabelled)"
     }
     return(consistency)
   })
-  colnames(finaldf) <- c("FilePath","GPR File","TIFF File","JPEG File","Num of lines","Consistent")
-  if (all(finaldf$Consistent == "TRUE")) {myFlag <- "PASS"} else {myFlag <- "FAIL"}
+  colnames(finaldf) <- c("FilePath","GPR File","TIFF File","JPEG File","F532/F635","QuoteMarksRemoved","BlankLinesRemoved","Num of lines","Consistency")
+  if (all(finaldf$Consistency == "PASS")) {myFlag <- "PASS"} else {myFlag <- "FAIL"}
   myDate <- format(Sys.Date(), "%Y%m%d")
   outFileName <- paste0(input_path, "/", myDate,"_rppaGprConsistencyCheck-",myFlag,out_suffix,".xls")
   
