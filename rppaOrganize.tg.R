076a9b040f4fdce0dbea74d7f881167cc963aaf0
trgandhi <trgandhi1@gmail.com>
2024-05-09 12:37:13 -0500

    Create rppaOrganize.tg.R
    created script to reorganize gpr and jpg files to work with the normalization pipeline

---
+++ rppaOrganize.tg.R
@@ -0,0 +1,21 @@
+# organize the gpr and the jpg files into two big folders ----
+rppaOrganize <- function(input_path = ".") {
+  alldirs <- basename(list.dirs(path = input_path, recursive = F))
+  
+  gpr.dirs <- alldirs[grepl("_gpr$",alldirs)]
+  jpg.dirs <- alldirs[grepl("_image$",alldirs)]
+  
+  mylabel <- strsplit(gpr.dirs[1], "_")[[1]][1]
+  
+  if (!dir.exists(file.path(input_path, paste0(mylabel,"_gpr")))) {
+    dir.create(file.path(input_path, paste0(mylabel,"_gpr")))
+  }
+  if (!dir.exists(file.path(input_path, paste0(mylabel,"_jpg")))) {
+    dir.create(file.path(input_path, paste0(mylabel,"_jpg")))
+  }
+  
+  file.rename(gpr.dirs, file.path(input_path, paste0(mylabel,"_gpr"), gpr.dirs))
+  file.rename(jpg.dirs, file.path(input_path, paste0(mylabel,"_jpg"), jpg.dirs))
+}
+
+rppaOrganize()
