138d47d752e9e9bb00f6475f0d9243b10b7caac2
trgandhi <trgandhi1@gmail.com>
2025-02-27 10:37:49 -0600

    Update 7.createConfigFile.tg.R
    updated script to automatically check if cellmix2 and cal2 samples are present in sample list or not and create config file accordingly

--- 7.createConfigFile.tg.R
+++ 7.createConfigFile.tg.R
@@ -14,6 +14,39 @@ mainFunction <- function(inputFile, rawConfigFile, outputFile) {
   prep.config[3] <- paste0("pi_list\t",paste(all.pis, collapse = "\t"),"\tCal\tCtrl")
   
   mydf.samplelist <- read.xlsx(inputFile, sheet = 1, startRow = 2, check.names = F)
+  idx.to.include <- c(1,2,5)
+  
+  # if cal_2 samples are not present in sample list, create config file accordingly
+  if (!(any(grepl("^Cal_2",mydf.samplelist$SampleName, ignore.case = T)))) {
+    for (idx in 1:length(prep.config)) {
+      if (idx %in% idx.to.include) {
+        temp1 <- strsplit(prep.config[idx], "\t")[[1]]
+        temp2 <- temp1[grep("cal_2", temp1, ignore.case = T, invert = T)]
+        prep.config[idx] <- paste(temp2, collapse = "\t")
+      }
+    }
+    prep.config[7] <- 'Cal_HP\tCal_1_HP'
+    prep.config[8] <- 'Cal_JC\tCal_1_JC'
+    prep.config[9] <- 'pi_cellmix\t"Cellmix2,Ctrl_Cellmix2_"'
+    prep.config[10] <- 'pi_Cal_HP\tCal_1_HP'
+    prep.config[11] <- 'pi_Cal_JC\tCal_1_JC'
+  }
+  
+  # if cellmix2 samples not present in sample list, remove cellmix2 and other qc2 samples from the configuration file
+  if (!(any(grepl("cellmix2",mydf.samplelist$SampleName, ignore.case = T)))) {
+    for (idx in 1:length(prep.config)) {
+      if (idx %in% idx.to.include) {
+        temp1 <- strsplit(prep.config[idx], "\t")[[1]]
+        temp2 <- temp1[grep("mix2|cal_2|buffer2", temp1, ignore.case = T, invert = T)]
+        prep.config[idx] <- paste(temp2, collapse = "\t")
+      }
+    }
+    prep.config[7] <- 'Cal_HP\tCal_1_HP'
+    prep.config[8] <- 'Cal_JC\tCal_1_JC'
+    prep.config[9] <- 'pi_cellmix\t"Cellmix1,Ctrl_Cellmix1_"'
+    prep.config[10] <- 'pi_Cal_HP\tCal_1_HP'
+    prep.config[11] <- 'pi_Cal_JC\tCal_1_JC'
+  }
   
   keep <- mydf.samplelist$Sample.Type == "M"
   if (any(keep)) { # check if there is at least 1 mouse sample present
@@ -21,6 +54,7 @@ mainFunction <- function(inputFile, rawConfigFile, outputFile) {
     mouse.pis <- unique(unname(sapply(mydf.samplelist[,2],function(x){strsplit(x,"_")[[1]][1]}))) # extract initials for pis with mouse samples
     if (!(all(grepl("ctrl",mouse.pis,ignore.case = T)))) { # check if it's not only control sample(s)
       mouse.pis <- mouse.pis[!(grepl("ctrl",mouse.pis,ignore.case = T))] # drop "Ctrl" from pi list
+      mouse.pis <- mouse.pis[order(mouse.pis, decreasing = F)] # sorting pi initials in ascending order
       prep.config[4] <- paste0("mouse_pi_list\t",paste(mouse.pis, collapse = "\t"))
     } else {
       prep.config[4] <- paste0("mouse_pi_list")
